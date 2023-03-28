60cde8f13c440f71053140faf774b7be9802f2e6
Philipp Schaad <schaad.phil@gmail.com>
2023-03-28 19:06:09 +0200

    Add init files

--- fuzzyflow/__init__.py
+++ fuzzyflow/__init__.py
@@ -6,3 +6,4 @@ from .verification.verifier import TransformationVerifier, StatusLevel
 from .cutout import CutoutStrategy
 from .verification.sampling import SamplingStrategy
 from .util import load_transformation_from_file
+from .harness_generator import sdfg2cpp
